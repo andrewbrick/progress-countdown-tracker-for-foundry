cb324045fdd6cb96e5c4f463ecf7587a7b18316f
epicname <andrew.brick@gmail.com>
2025-08-02 18:00:55 -0700

    Update tictac-tracker.js

--- tictac-tracker.js
+++ tictac-tracker.js
@@ -1,1 +1,76 @@
 
+Hooks.once("init", () => {
+
+  // progress pip character
+  game.settings.register("tictac-tracker", "progressPipCharacter", {
+    name: "Progress Pip Character",
+    hint: "Change the pip character for progress trackers (use one character only). Applies only to you.",
+    scope: "client",
+    config: true,
+    type: String,
+    default: "\u25CF", // unicode black circle
+    onChange: async (value) => {
+      // Validate single char
+      const trimmed = value.trim();
+      if (trimmed.length === 0) {
+        ui.notifications.warn("Progress Pip Character is blank. Using default.");
+        await game.settings.set("tictac-tracker", "progressPipCharacter", "●");
+      } else if (trimmed.length > 1) {
+        const firstChar = trimmed.charAt(0);
+        ui.notifications.warn("You must enter exactly one character. Using only the first character.");
+        await game.settings.set("tictac-tracker", "progressPipCharacter", firstChar);
+      }
+      reRender();
+    }
+  });
+  
+  // consequence pip character
+  game.settings.register("tictac-tracker", "consequencePipCharacter", {
+    name: "Consequence Pip Character",
+    hint: "Change the pip character for consequence trackers (use one character only). Applies only to you.",
+    scope: "client",
+    config: true,
+    type: String,
+    default: "\u25CF", // unicode black circle
+    onChange: async (value) => {
+      // Validate single char
+      const trimmed = value.trim();
+      if (trimmed.length === 0) {
+        ui.notifications.warn("Consequence Pip Character field is blank. Using default.");
+        await game.settings.set("tictac-tracker", "consequencePipCharacter", "●");
+      } else if (trimmed.length > 1) {
+        const firstChar = trimmed.charAt(0);
+        ui.notifications.warn("You must enter exactly one character. Using only the first character.");
+        await game.settings.set("tictac-tracker", "consequencePipCharacter", firstChar);
+      }
+      reRender();
+    }
+  });
+
+  // progress pip color
+  game.settings.register("tictac-tracker", "progressPipColor", {
+    name: "Progress Pip Color",
+    hint: "Change the color of active pips for progress trackers. Applies only to you.",
+    scope: "client",
+    config: true,
+    type: new game.colorPicker.ColorPickerField(),
+    default: "#A02B93",
+    onChange: () => {
+      reRender();
+    }
+  });
+
+  // consequence pip color
+  game.settings.register("tictac-tracker", "consequencePipColor", {
+    name: "Progress Pip Color",
+    hint: "Change the color of active pips for consequence trackers. Applies only to you.",
+    scope: "client",
+    config: true,
+    type: new game.colorPicker.ColorPickerField(),
+    default: "#A02B93",
+    onChange: () => {
+      reRender();
+    }
+  });
+  
+} // end init
